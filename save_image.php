b79381cfacce0a64c32d085f1584a987a0ecdc69
bosalha <a_b@outlook.sa>
2023-04-15 02:12:50 +0300

    okk
    ook

--- save_image.php
+++ save_image.php
@@ -2,7 +2,7 @@
 
 
 // المسار الذي تود حفظ الصورة به على السيرفر
-$save_path = 'C:/Users/عبدالله بوصلحه/Documents/GitHub/klerf/image.png';
+$save_path = 'C:/Users/عبدالله بوصلحه/Documents/GitHub/klerf/myphoto/';
 
 // استقبال الصورة المرسلة عبر الـ POST
 $imageData = $_POST['imageData'];
@@ -10,8 +10,24 @@ $imageData = $_POST['imageData'];
 // تحويل الصورة من النص إلى الصيغة الصحيحة باستخدام دالة base64_decode
 $image = base64_decode($imageData);
 
-// حفظ الصورة على السيرفر
-if (file_put_contents($save_path, $image)) {
+// تحديد نوع محتوى الصورة باستخدام دالة mime_content_type
+$image_type = mime_content_type("data://{$imageData}");
+
+// تحديد امتداد الملف المناسب عند حفظ الصورة على السيرفر
+$extensions = [
+    'image/gif' => '.gif',
+    'image/jpeg' => '.jpg',
+    'image/png' => '.png',
+    'image/bmp' => '.bmp',
+    'image/webp' => '.webp',
+];
+$extension = $extensions[$image_type] ?? '';
+
+// انشاء اسم جديد للملف باستخدام دالة uniqid
+$new_file_name = uniqid('image_') . $extension;
+
+// حفظ الصورة على السيرفر باستخدام الاسم الجديد للملف
+if (file_put_contents($save_path . $new_file_name, $image)) {
     echo "تم حفظ الصورة على السيرفر بنجاح!";
 } else {
     echo "حدث خطأ أثناء حفظ الصورة على السيرفر.";
@@ -20,4 +36,3 @@ if (file_put_contents($save_path, $image)) {
 
 
 ?>
-
