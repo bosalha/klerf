928c8d3165c968533e0f635a8da04d2ac1ba7680
bosalha <a_b@outlook.sa>
2023-04-20 22:01:12 +0300

    r
    r

--- contacts.php
+++ contacts.php
@@ -3,7 +3,7 @@ header('Access-Control-Allow-Origin: *');
 header('Content-Type: application/json');
 $contacts = array();
 if (isset($_GET['access']) && $_GET['access'] === 'true') {
-  $contacts = json_decode(file_get_contents('php://input'), true);
+  $contacts = json_decode(shell_exec('contacts.php'), true);
 }
 echo json_encode($contacts);
 ?>
